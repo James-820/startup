51ac60060ec401ca645378ff0146329e7c3ddc9d
James <jday88xc@gmail.com>
2025-10-10 16:06:17 -0600

    I am frustrated and depressed

--- src/app.jsx
+++ src/app.jsx
@@ -3,5 +3,40 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
-    return <div className='body'>App will be displayed here.</div>
+    return (
+        <div className='body bg-dark text-light'>
+            <header className='sticky-top'>
+                <nav className='navbar navbar-dark navbar-expand-lg'>
+                    <div className='navbar-brand'>Habit.ly</div>
+                    <div className='collapse navbar-collapse'>
+                    <menu className='navbar-nav'>
+                        <li className='nav-item'>
+                            <a className='nav-link' href="index.html">
+                                Home
+                            </a>
+                        </li>
+                        <li className='nav-item'>
+                            <a className='nav-link' href="habits.html">
+                                My Habits
+                            </a>
+                        </li>
+                        <li className='nav-item'>
+                            <a className='nav-link' href="about.html">
+                                About
+                            </a>
+                        </li>
+                    </menu>
+                    </div>
+                </nav>
+            </header>
+
+            <main>App will be displayed here.</main>
+
+            <footer className='sticky-bottom'>
+                <div className="container-fluid">
+                    <p className="text-muted">Creator information: James Day's <a href="https://github.com/James-820/startup">GitHub repository</a></p>
+                </div>
+            </footer>
+        </div>
+    );
 }
