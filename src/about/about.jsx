e23cd89a40084fc36489f0eb53be436f4c3aa525
James <jday88xc@gmail.com>
2025-10-17 12:07:28 -0600

    update(habits.css, login.css, login.jsx, about.jsx) Fixed some CSS issues

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -5,31 +5,31 @@ export function About() {
     return (
         <main>
             <h2 id="about-heading">About <span>Habit.ly</span></h2>
-            <div class="card">
-                <div class="card-body">
-                    <p class="card-text">
+            <div className="card">
+                <div className="card-body">
+                    <p className="card-text">
                         Habit.ly was first created by James Day, then a student at Brigham Young University in Provo, Utah. His goal was to create a 
                         habit tracker that would leverage group accountability to support people in their quest for progress. Later, he got super, 
                         super rich, and now lives on a private island somewhere where he can make more web apps and paint Warhammer minis in peace (or at 
                         least that's what he's manifesting for himself).
                     </p>
-                    <p class="card-text">
+                    <p className="card-text">
                         If you have questions or need customer support, that's really too bad. We don't have any of that stuff yet. Just be patient for now. 
                         In the meantime, here's a quote:
                     </p>
                 </div>
             </div>
 
-            <div class="card">
-                <div class="card-body">
-                    <p class="card-text" id="inspiring-quote">
+            <div className="card">
+                <div className="card-body">
+                    <p className="card-text" id="inspiring-quote">
                         "Yesterday is history, tomorrow is a mystery, and today is a gift... that is why it is called the 'present'." 
                         -- Master Oogway, from Kung Fu Panda
                     </p>
                 </div>
             </div>
 
-            <div class="pic-frame">
+            <div className="pic-frame">
                 <img id="random-pic" src="/placeholder.png" alt="Random cool picture" />
             </div>
 
--- src/login/login.jsx
+++ src/login/login.jsx
@@ -4,13 +4,13 @@ import './login.css';
 export function Login() {
     return (
         <main>
-            <div class="card">
-                <img class="card-img-top" src="placeholder.png" alt="Placeholder image" />
-                <div class="card-body">
-                    <h5 class="card-title">Login</h5>
+            <div className="card">
+                <img className="card-img-top" src="placeholder.png" alt="Placeholder image" />
+                <div className="card-body">
+                    <h5 className="card-title">Login</h5>
                     <input id="username" type="text" placeholder="Username or Email"/>
                     <input id="password" type="password" placeholder="Password" />
-                    <div id="submit" class="btn">Submit</div>
+                    <div id="submit" className="btn">Submit</div>
                 </div>
             </div>
         </main>
