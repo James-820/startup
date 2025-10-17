af126421cd2b037dd762ddbecd5a51d6eac2ff6e
James <jday88xc@gmail.com>
2025-10-17 13:57:29 -0600

    update(about.css, habits.css, habits.jsx, login.css, app.css) Completed basic layout for Habits component

--- src/habits/habits.jsx
+++ src/habits/habits.jsx
@@ -3,6 +3,41 @@ import './habits.css';
 
 export function Habits () {
     return (
-        <main>Habit information displayed on this page.</main>
+        <main>
+            <div id="habits-bar" className="page-section">
+                <div id="summary-btn" className="btn">Show Summary</div>
+                <div className="card habit">
+                    <div className="card-body">
+                        <h5 className="card-title">Make Bed</h5>
+                        <div className="card-text">
+                            <ul>
+                                <li>Streak: 7</li>
+                                <li>Completed: When all blankets are folded</li>
+                                <li>Share to: None</li>
+                            </ul>
+                        </div>
+                        <div className="btn">Mark as Complete</div>
+                    </div>
+                </div>
+
+                <div className="card habit">
+                    <div className="card-body">
+                        <h5 className="card-title">Water Plants</h5>
+                        <div className="card-text">
+                            <ul>
+                                <li>Streak: 14</li>
+                                <li>Completed: When all plants are watered</li>
+                                <li>Share to: Group 1</li>
+                            </ul>
+                        </div>
+                        <div className="btn">Mark as Complete</div>
+                    </div>
+                </div>
+            </div>
+
+            <div id="summary-bar" className="page-section">
+                Summary info here
+            </div>
+        </main>
     );
 }
